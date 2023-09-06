6e4bf94d606ea3a98cfa1e4a270a6a1549e9afd1
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-09-06 08:54:35 -0700

    Update PlayerCombat.cs

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers; 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //animator.SetTrigger("Attack");
+
+        Physics2D.OverlapCircleAll(); 
     }
 }
