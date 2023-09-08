53846e4bfae9c5dc5967008b856383a993844ae5
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-09-08 08:13:39 -0700

    Update LevelLoader.cs

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -14,10 +14,9 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            LoadNextLevel(); 
-        }
+        
+         //LoadNextLevel(); 
+        
     }
 
     public void LoadNextLevel()
