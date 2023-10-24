aa52ffa1de3ffb0e31841c72d340b3b0b9b03309
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-10-24 10:28:46 -0700

    Boss AI

---
+++ Assets/Scripts/BossScripts/BossShoot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoot : MonoBehaviour
+{
+    public GameObject player; 
+    public GameObject blueBullet;
+    public GameObject purpleBullet;
+    public GameObject redBullet;
+    public Transform blueSpawnPoint;
+    public Transform purpleSpawnPoint;
+    public Transform redSpawnPoint;
+    private GameObject blueBulletSpawn;
+    private GameObject purpleBulletSpawn;
+    private GameObject redBulletSpawn;
+    public float bulletSpeed = 1.5f; 
+
+
+    public void Shoot()
+    {
+        Debug.Log("Successful Shot");
+        Vector2 fireDirection = (player.transform.position - transform.position);
+        fireDirection.Normalize();
+        Vector2 blueSpawn = blueSpawnPoint.position;
+        Vector2 purpleSpawn = purpleSpawnPoint.position;
+        Vector2 redSpawn = redSpawnPoint.position;
+
+        blueBulletSpawn = Instantiate(blueBullet, blueSpawn, Quaternion.identity);
+        purpleBulletSpawn = Instantiate(purpleBullet, purpleSpawn, Quaternion.identity);
+        redBulletSpawn = Instantiate(redBullet, redSpawn, Quaternion.identity);
+        blueBulletSpawn.GetComponent<Rigidbody2D>().velocity = new Vector2(fireDirection.x * bulletSpeed, 0);
+        purpleBulletSpawn.GetComponent<Rigidbody2D>().velocity = new Vector2(fireDirection.x * bulletSpeed, 0);
+        redBulletSpawn.GetComponent<Rigidbody2D>().velocity = new Vector2(fireDirection.x * bulletSpeed, 0);
+    }
+}
--- Assets/StateBehaviour/Orb_Fly.cs
+++ Assets/StateBehaviour/Orb_Fly.cs
@@ -7,26 +7,43 @@ public class Orb_Fly : StateMachineBehaviour
 
     Transform player;
     Rigidbody2D rb;
+    FirstBoss boss; 
     public float speed = 2.5f;
+    public float attackRange = 1.5f;
+    public float minimumDistance;
+    public Transform bossLocation; 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>(); 
+        boss = animator.GetComponent<FirstBoss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        boss.LookAtPlayer();
+
         Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2 newPosition = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-        rb.MovePosition(newPosition);
+        if (Vector2.Distance(bossLocation.position, player.position ) < minimumDistance)
+        {
+            Vector2 newPosition = Vector2.MoveTowards(rb.position, target, -speed * Time.fixedDeltaTime);
+            rb.MovePosition(newPosition);
+        }
+        //Vector2 newPosition = Vector2.MoveTowards(rb.position, target, -speed * Time.fixedDeltaTime);
+        //rb.MovePosition(newPosition);
+
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        animator.ResetTrigger("Attack");
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
